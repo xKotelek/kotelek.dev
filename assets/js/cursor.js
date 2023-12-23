de5dd397fdb9c9961dc6e617df6a28ef4aa337c8
xKotelek <contact@kotelek.dev>
2023-12-23 14:01:30 +0100

    Co-authored-by: yacoreq <yacoreq@users.noreply.github.com>

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,5 +1,7 @@
 const cursor = document.getElementById("cursor");
+const cursor1 = document.querySelector('.cursor');
 const container = document.querySelector(".container");
+const memoji = document.querySelector('.memoji-animated');
 
 if (/Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent)) {
   console.log('Website visited on mobile!');
@@ -12,24 +14,40 @@ if (/Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent)) {
       left: `${clientX}px`,
       top: `${clientY}px`
     }, { duration: 3000, fill: "forwards" });
+
+    const memojiRect = memoji.getBoundingClientRect();
+    const rotateX = (clientY - memojiRect.top - memojiRect.height / 2) / -9;
+    const rotateY = (clientX - memojiRect.left - memojiRect.width / 2) / -16.5;
+
+    memoji.animate({
+      transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    }, { duration: 2000 });
   }
 }
 
 function cursorFocus() {
+  cursor1.classList.add("focused")
   cursor.classList.add("focused");
   container.classList.add("cursor-focused");
 }
 function cursorUnFocus() {
+  cursor1.classList.remove("focused")
   container.classList.remove("cursor-focused");
   cursor.classList.remove("focused");
 }
 
-const cursor1 = document.querySelector('.cursor');
-
 const positionElement = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;  
-  cursor1.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  if (/Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent)) {
+    console.log('Website visited on mobile!');
+    cursor1.remove();
+  } else {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+    windowheigh = window.screen.height;
+    windowwidth = window.screen.width;
+    ambient = 1;
+    cursor1.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  }
 }
 
 window.addEventListener('mousemove', positionElement)
