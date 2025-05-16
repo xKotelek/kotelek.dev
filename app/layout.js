e2c0ed14e008f1eba7186e59290401fa63b6b2aa
xKotelek <contact@kotelek.dev>
2025-05-16 22:22:57 +0200

    metadata update

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta property="og:title" name="twitter:title" itemprop="name" content="kotelek.dev" />
+        <meta property="og:site_name" content="kotelek.dev" />
         <meta name="theme-color" content="#ad46ff" />
+        <meta property="og:description"  name="twitter:description" itemprop="description" content="xKotelek • Frontend & backend developer." />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" name="twitter:image:src" itemprop="image" content="https://kotelek.dev/memoji.png" />
+        <meta property="og:image:width" content="128" />
+        <meta property="og:image:height" content="128" />
+        <meta property="og:url" content="https://www.kotelek.dev" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image:alt" name="twitter:image:alt" content="There should be logo"/>
+
         <link rel="icon" href="/favicon.ico" />
         <script src="https://kit.fontawesome.com/681f16d40f.js" crossOrigin="anonymous"></script>
       </head>
